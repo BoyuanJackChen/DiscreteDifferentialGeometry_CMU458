235f6fc75d245fb0fca0fbc5ebdfa7f42e4ff45e
Boyuan Chen <boyuanchen@Boyuans-MacBook-Air.local>
2020-02-04 20:48:28 -0800

    Finished star and closure

--- projects/simplicial-complex-operators/simplicial-complex-operators.js
+++ projects/simplicial-complex-operators/simplicial-complex-operators.js
@@ -25,18 +25,7 @@ class SimplicialComplexOperators {
      * @param {module:Core.Mesh} mesh The input mesh which we index.
      */
     assignElementIndices(mesh) {
-        var i;
-        for (i = 0; i < mesh.vertices.length; i++) {
-            mesh.vertices[i].index=i;
-        }
-        var j;
-        for (j = 0; j < mesh.edges.length; j++) {
-            mesh.edges[j].index=j;
-        }
-        var k;
-        for (k = 0; k < mesh.edges.length; k++) {
-            mesh.faces[k].index=k;
-        }
+		this.mesh.indexElements();
     }
 
     /** Returns the vertex-edge adjacency matrix of the given mesh.
@@ -45,7 +34,19 @@ class SimplicialComplexOperators {
      * @returns {module:LinearAlgebra.SparseMatrix} The vertex-edge adjacency matrix of the given mesh.
      */
     buildVertexEdgeAdjacencyMatrix(mesh) {
-            // TODO
+		// Initialize the vertex-edge matrix. First layer is edge; second layer is vertices.
+		var matrix = new Array(mesh.edges.length)
+		for (var i = 0; i < mesh.edges.length; i++) {
+			matrix[i] = new Array(mesh.vertices.length).fill(0);
+		}
+		// For each vertex, iterate through the halfedges. 
+        for (var i = 0; i < mesh.vertices.length; i++) {
+            var vhi = new VertexHalfedgeIterator(mesh.vertices[i].halfedge);
+			for(let thisHalfEdge of vhi){
+				matrix[thisHalfEdge.edge.index][mesh.vertices[i].index] = 1
+			}
+        }
+		return matrix;
     }
 
     /** Returns the edge-face adjacency matrix.
@@ -54,7 +55,19 @@ class SimplicialComplexOperators {
      * @returns {module:LinearAlgebra.SparseMatrix} The edge-face adjacency matrix of the given mesh.
      */
     buildEdgeFaceAdjacencyMatrix(mesh) {
-            // TODO
+		// Initialize the vertex-edge matrix. First layer is faces; second layer is edges.
+		var matrix = new Array(mesh.faces.length)
+		for (var i = 0; i < mesh.faces.length; i++) {
+			matrix[i] = new Array(mesh.edges.length).fill(0);
+		}
+		// For each face, iterate through the halfedges. 
+        for (var i = 0; i < mesh.faces.length; i++) {
+            var fhi = new FaceHalfedgeIterator(mesh.faces[i].halfedge);
+			for(let thisHalfEdge of fhi){
+				matrix[mesh.faces[i].index][thisHalfEdge.edge.index] = 1
+			}
+        }
+		return matrix;
     }
 
     /** Returns a column vector representing the vertices of the
@@ -65,7 +78,14 @@ class SimplicialComplexOperators {
      *  vertex i is in the given subset and 0 otherwise
      */
     buildVertexVector(subset) {
-            // TODO
+		var vector = new Array(mesh.vertices.length).fill(0);
+		console.log('Subset number of vertices: ');
+		console.log(subset.vertices.size);
+		for(let v_index of subset.vertices) {
+			// console.log(Object.prototype.toString.call(v_index));
+			vector[v_index] = 1;
+		}
+		return vector;
     }
 
     /** Returns a column vector representing the edges of the
@@ -76,7 +96,13 @@ class SimplicialComplexOperators {
      *  edge i is in the given subset and 0 otherwise
      */
     buildEdgeVector(subset) {
-            // TODO
+		var vector = new Array(mesh.edges.length).fill(0);
+		console.log("Subset has this many edges: ");
+		console.log(subset.edges.size);
+		for(let e_index of subset.edges) {
+			vector[e_index] = 1;
+		}
+		return vector;
     }
 
     /** Returns a column vector representing the faces of the
@@ -87,8 +113,39 @@ class SimplicialComplexOperators {
      *  face i is in the given subset and 0 otherwise
      */
     buildFaceVector(subset) {
-            // TODO
+		var vector = new Array(mesh.faces.length).fill(0);
+		console.log("Subset has this many faces: ");
+		console.log(subset.faces.size);
+		for(let f_index of subset.faces) {
+			vector[f_index] = 1;
+		}
+		return vector;
     }
+
+	// Debugging function
+	print2DMatrix(matrix) {
+  		for(var z = 0; z < matrix.length; z++) {
+			for(var i = 0; i < matrix[0].length; i++) {
+    			console.log(matrix[z][i]);
+  			}
+		}
+	}
+
+	// Debugging function
+	print1Entries(matrix) {
+		var accum = 0;
+		console.log("The 1 entries are: ");
+		for(var z = 0; z < matrix.length; z++) {
+			for(var i = 0; i < matrix[0].length; i++) {
+				if (matrix[z][i]==1) {
+    				console.log(z,i);
+					accum++;
+				}
+  			}
+		}
+		console.log(accum/2);
+		console.log("End of 1 entries");
+	}
 
     /** Returns the star of a subset.
      * @method module:Projects.SimplicialComplexOperators#star
@@ -96,9 +153,40 @@ class SimplicialComplexOperators {
      * @returns {module:Core.MeshSubset} The star of the given subset.
      */
     star(subset) {
-            // TODO
+		// The star should at least have all the original components
+		var result = subset;  
+		var vertexVector = this.buildVertexVector(subset);
+		var edgeVector = this.buildEdgeVector(subset);
 
-            return subset; // placeholder
+		// Process vertices
+		for (var i = 0; i < vertexVector.length; i++) {
+			if (vertexVector[i]==1) {
+				// Add related edges
+				var vei = new VertexEdgeIterator(this.mesh.vertices[i].halfedge);
+				for (let edge of vei) {
+					result.addEdge(edge.index);
+				}
+				// Add related faces
+				var vfi = new VertexFaceIterator(this.mesh.vertices[i].halfedge);
+				for (let face of vfi) {
+					result.addFace(face.index);
+				}
+			}
+		}
+		// Process edges
+		for (var i = 0; i < edgeVector.length; i++) {
+			if (edgeVector[i]==1) {
+				// Add related faces
+				for (var j = 0; j < this.A1.length; j++) {
+					if (this.A1[j][i]==1){
+						result.addFace(this.mesh.faces[j].index);
+					}
+				}
+			}
+		}
+		// Finally, Faces don't generate new components for star.
+		// So we don't need to process faces. 
+        return result;
     }
 
     /** Returns the closure of a subset.
@@ -107,9 +195,35 @@ class SimplicialComplexOperators {
      * @returns {module:Core.MeshSubset} The closure of the given subset.
      */
     closure(subset) {
-            // TODO
+		// The closure should at least have all the original components
+		var result = subset; 
+		var edgeVector = this.buildEdgeVector(subset); 
+		var faceVector = this.buildFaceVector(subset);
+		// Vertices do not lead to extra elements in closre. 
 
-            return subset; // placeholder
+        // Process edges by adding two related vertices
+		for (var i = 0; i < edgeVector.length; i++) {
+			if (edgeVector[i]==1) {
+				var v1_index = this.mesh.edges[i].halfedge.vertex.index;
+				result.addVertex(v1_index);
+				var v2_index = this.mesh.edges[i].halfedge.next.vertex.index;
+				result.addVertex(v2_index); 
+			}
+		}
+		// Process faces by adding related edges and vertices
+		for (var i = 0; i < faceVector.length; i++) {
+			if (faceVector[i]==1) {
+				var vertices  = this.mesh.faces[i].adjacentVertices();
+				var edges = this.mesh.faces[i].adjacentEdges();
+				for (let v of vertices) {
+					result.addVertex(v.index);
+				}
+				for (let e of edges) {
+					result.addEdge(e.index);
+				}
+			}
+		}
+        return result; 
     }
 
     /** Returns the link of a subset.
